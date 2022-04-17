bbe45fcaff2abc56c6d29eeed39ca2d84c7f953f
Shahjalal-Fahad <shahjalalptw@gmail.com.com>
2022-04-17 16:23:04 +0600

    extra part

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -4,7 +4,27 @@ const Extra = () => {
     return (
         <div>
             <h2>Useful info and health tips
-</h2>
+    </h2>
+    <div>
+        <h3> Cardiac electrophysiology</h3>
+        <p>An electrophysiology (EP) study is a test performed to assess your heart's electrical system or activity and is used to diagnose abnormal heartbeats or arrhythmia. The test is performed by inserting catheters and then wire electrodes, which measure electrical activity, through blood vessels that enter the heart. </p>
+    </div>
+    <div>
+        <h3>Echocardiography </h3>
+        <p> 
+An echocardiogram checks how your heart's chambers and valves are pumping blood through your heart. An echocardiogram uses electrodes to check your heart rhythm and ultrasound technology to see how blood moves through your heart. An echocardiogram can help your doctor diagnose heart conditions.</p>
+    </div>
+    <div>
+        <h3>Interventional cardiology
+ </h3>
+        <p> The most common interventional cardiology procedure (catheterization) involves the insertion of a sheath into the groin or wrist artery and passing a catheter to the heart to open a blocked artery with balloon (angioplasty) or a stent (metal sleeve that is inserted to prop open the artery from within).</p>
+    </div>
+    <div>
+        <h3> Nuclear cardiology
+</h3>
+        <p> 
+Nuclear cardiology studies use noninvasive techniques to assess myocardial blood flow, evaluate the pumping function of the heart as well as visualize the size and location of a heart attack. Among the techniques of nuclear cardiology, myocardial perfusion imaging is the most widely used.</p>
+    </div>
 
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Carrosel from '../Carrosel/Carrosel';
 import Description from '../Description/Description';
+import Extra from '../Extra/Extra';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
              <Carrosel/>
-             <Description/>
+             <Services/>
+             <Extra/>
+             {/* <Description/> */}
         </div>
     );
 };
